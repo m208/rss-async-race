2032c02a2e0e38be86b3c4219f17c5133bf2c25f
m208 <avcvet@yandex.ru>
2022-08-07 16:04:36 +0300

    feat: add homepage var for correct routing

--- async-race/src/App.tsx
+++ async-race/src/App.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+
 import { Garage } from './components/Garage';
 import { Navigation } from './components/Navigation';
 import { WinnersTable } from './components/WinnersTable';
 import { createPagState } from './context/appState';
-import { IAppState, TSortDir, TSortOptions } from './types/types';
+import { ICarState } from './model/Car';
+import { model } from './model/fetchData';
+import { IAnimState, IAppState, ICar, ICarSpeed, TEngineStatus, TSortDir, TSortOptions } from './types/types';
 
+const homepage = '/m208-JSFE2022Q1/async-race';
 
 function App() {
   const garageCurrPage = useState(1);
@@ -19,18 +23,26 @@ function App() {
   const [timeSorter, setTimeSorter] = useState<TSortDir>('Asc');
   const [winSorter, setWinSorter] = useState<TSortDir>('Asc');
 
+  const [counter, setCounter] = React.useState(0);
+  const [shouldAnimate, setShouldAnimate] = React.useState(true);
+
+  const [carState, setCarState] = useState<Array<ICarState>>([]);
+
   const appState: IAppState = {
     garagePagState: createPagState(garageCurrPage, garageLastPage),
     winnersPagState: createPagState(winnersCurrPage, winnersLastPage),
-    winnersSort: { sortBy, setSortBy, activeSorter, setActiveSorter, timeSorter, setTimeSorter, winSorter, setWinSorter, onChange: ()=>{} },
+    winnersSort: { sortBy, setSortBy, activeSorter, setActiveSorter, timeSorter, setTimeSorter, winSorter, setWinSorter, onChange: () => { } },
+
+    //animationCount: prepareState({ counter, setCounter, shouldAnimate, setShouldAnimate, startAnim: ()=>{} }),
+    //carState: { carState, setCarState },
   };
 
   return (
     <>
       <Navigation />
       <Routes>
-        <Route path='/' element={<Garage appState={appState} />}></Route>
-        <Route path='/winners' element={<WinnersTable appState={appState} />}></Route>
+        <Route path={`${homepage}/`} element={<Garage appState={appState} />}></Route>
+        <Route path={`${homepage}/winners/`} element={<WinnersTable appState={appState} />}></Route>
       </Routes>
     </>
   );
--- async-race/src/components/Navigation.tsx
+++ async-race/src/components/Navigation.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const homepage = '/m208-JSFE2022Q1/async-race';
+
 export function Navigation() {
   return (
     <nav className="app_menu">
       <span>
-        <Link to="/">
-            <button>Garage</button>
+        <Link to={homepage}>
+            <button>Garage1</button>
         </Link>
-        <Link to="/winners">
-            <button>Winners</button>
+        <Link to={`${homepage}/winners`}>
+            <button>Winners1</button>
         </Link>
       </span>
     </nav>
