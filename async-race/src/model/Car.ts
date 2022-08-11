9c77d48efc446d42dc3db6a6295cf951ac022c08
m208 <avcvet@yandex.ru>
2022-08-11 15:07:28 +0300

    refactor: remove useless code from car model

--- async-race/src/model/Car.ts
+++ async-race/src/model/Car.ts
@@ -6,26 +6,14 @@ export interface ICarState {
   id: number;
   color: string;
   name: string;
-  car: ICar;
-  setCar: React.Dispatch<React.SetStateAction<ICar>>;
   broken: boolean;
-  setBroken: React.Dispatch<React.SetStateAction<boolean>>;
   carSpeed: number;
-  setCarSpeed: React.Dispatch<React.SetStateAction<number>>;
   engineStatus: TEngineStatus;
-  setEngineStatus: React.Dispatch<React.SetStateAction<TEngineStatus>>;
   duration: number;
-  setDuration: React.Dispatch<React.SetStateAction<number>>;
   shouldAnimate: boolean;
-  setshouldAnimate: React.Dispatch<React.SetStateAction<boolean>>;
-  
   finished: boolean;
-  setFinished: React.Dispatch<React.SetStateAction<boolean>>;
   awaiting: boolean;
-  setAwaiting: React.Dispatch<React.SetStateAction<boolean>>;
   reseted: boolean;
-  setReseted: React.Dispatch<React.SetStateAction<boolean>>;
-  raceCounter:number
 
   reset: () => void;
   broke: () => void;
@@ -37,8 +25,9 @@ export interface ICarState {
 }
 
 export function getCarStateObject(c: ICar) {
-  const [broken, setBroken] = useState(false);
   const [car, setCar] = useState(c);
+
+  const [broken, setBroken] = useState(false);
   const [carSpeed, setCarSpeed] = useState(0);
   const [engineStatus, setEngineStatus] = useState<TEngineStatus>('stopped');
   const [duration, setDuration] = useState(0);
@@ -46,22 +35,23 @@ export function getCarStateObject(c: ICar) {
   const [finished, setFinished] = useState(false);
   const [awaiting, setAwaiting] = useState(false);
   const [reseted, setReseted] = useState(false);
-  const [raceCounter, setRaceCounter] = useState(-1);
+
 
   const carObj = {
     id: car.id,
     color: car.color,
     name: car.name,
 
-    duration, setDuration,
-    shouldAnimate, setshouldAnimate,
-    finished, setFinished,
-    awaiting, setAwaiting,
-    reseted, setReseted, raceCounter,
-    car, setCar, broken, setBroken, carSpeed,
-    setCarSpeed, engineStatus, setEngineStatus,
+    duration,
+    shouldAnimate,
+    finished, 
+    awaiting, 
+    reseted,  
+    broken, 
+    carSpeed,
+    engineStatus, 
 
-    reset: async () => { 
+    reset: async () => {
       await carObj.stop();
       setshouldAnimate(false);
       setBroken(false);
@@ -71,10 +61,7 @@ export function getCarStateObject(c: ICar) {
 
     broke: () => { setBroken(true); },
 
-    finish: () => { 
-      setFinished(true); 
-
-    },
+    finish: () => { setFinished(true); },
 
     start: async () => {
       setReseted(false);
@@ -85,7 +72,6 @@ export function getCarStateObject(c: ICar) {
       setEngineStatus('started');
       const speed = (start.data as ICarSpeed).velocity;
       setDuration((start.data as ICarSpeed).distance / 1000 / speed);
-
       setCarSpeed(speed);
     },
 
@@ -112,8 +98,7 @@ export function getCarStateObject(c: ICar) {
       }
     },
 
-    ride: async (counter? :number) => {
-      if (counter) setRaceCounter(counter);
+    ride: async () => {
       await carObj.start();
       await carObj.drive();
     },
